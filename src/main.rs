5a6c4d1e693c23c86a9eedef720b03a08f2c2885
Siddharth Doshi <sidoshi510@gmail.com>
2025-09-27 13:04:31 +0200

    fetch hn upvoted submissions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{Client, Url, cookie::Jar};
+
+const HN_BASE_URL: &str = "news.ycombinator.com";
+
+fn get_hn_client(hn_auth: &str) -> anyhow::Result<Client> {
+    let cookie = format!("user={hn_auth}; Domain={HN_BASE_URL}");
+    let url = format!("https://{HN_BASE_URL}").parse::<Url>()?;
+
+    let jar = Jar::default();
+    jar.add_cookie_str(&cookie, &url);
+
+    let client = reqwest::Client::builder()
+        .cookie_provider(jar.into())
+        .build()?;
+
+    Ok(client)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
+    let hn_auth = dotenvy::var("HN_AUTH")?;
+    let client = get_hn_client(&hn_auth)?;
+
+    let body = client
+        .get("https://news.ycombinator.com/upvoted?id=sidoshi")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    dbg!(body);
+
+    Ok(())
 }
